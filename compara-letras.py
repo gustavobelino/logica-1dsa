a8d83b0673f644033a40fc22a5a1c2d2877333e6
gustavobelino <gustavo.belino.faria@escola.pr.gov.br>
2025-04-22 11:34:58 -0300

    Create compara-letras.py

---
+++ compara-letras.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário que insira dois letras
+letra1 = (input("Digite o 1o. letra: "))
+letra2 = (input("Digite o 2o. letra: "))
+
+# Verifica comparando se letras são iguais
+if letra1 == letra2:
+    print("Os letras são iguais.")
+else:
+    print("Os letras são diferentes.")
